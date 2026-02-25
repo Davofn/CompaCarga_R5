7e438b7d21f04f996f0d708ef1034f33f36d03b2
Davofn <davofn@gmail.com>
2026-02-25 23:24:39 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Simple offline cache (cache-first for app shell)
-const CACHE_NAME = "r5-cargadores-pwa-v6";
+const CACHE_NAME = "r5-cargadores-pwa-v7";
 const ASSETS = [
   "./",
   "./index.html",
