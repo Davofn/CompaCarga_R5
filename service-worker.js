55ecfa9b4e4c47137ca807a2afdd4232c360ecac
Davofn <davofn@gmail.com>
2026-04-20 09:36:34 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Simple offline cache (cache-first for app shell)
-const CACHE_NAME = "r5 cargadores v2.1.2";
+const CACHE_NAME = "r5 cargadores v2.1.3";
 const ASSETS = [
   "./",
   "./index.html",
